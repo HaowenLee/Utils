4f4af5d8082308b30d2a61ef1a824307dada4b14
haowen <haowenhello@gmial.com>
2017-09-25 10:12:37 +0800

    remoce applyDimens

--- utils/src/main/java/me/utils/SizeUtil.kt
+++ utils/src/main/java/me/utils/SizeUtil.kt
@@ -1,9 +1,5 @@
 package me.utils
 
-import android.util.DisplayMetrics
-import android.util.TypedValue
-
-
 object SizeUtil {
 
     fun dp2px(dpValue: Float): Int {
@@ -24,17 +20,5 @@ object SizeUtil {
     fun px2sp(pxValue: Float): Int {
         val fontScale = Utils.context.resources.displayMetrics.scaledDensity
         return (pxValue / fontScale + 0.5f).toInt()
-    }
-
-    fun applyDimension(unit: Int, value: Float, metrics: DisplayMetrics): Float {
-        when (unit) {
-            TypedValue.COMPLEX_UNIT_PX -> return value
-            TypedValue.COMPLEX_UNIT_DIP -> return value * metrics.density
-            TypedValue.COMPLEX_UNIT_SP -> return value * metrics.scaledDensity
-            TypedValue.COMPLEX_UNIT_PT -> return value * metrics.xdpi * (1.0f / 72)
-            TypedValue.COMPLEX_UNIT_IN -> return value * metrics.xdpi
-            TypedValue.COMPLEX_UNIT_MM -> return value * metrics.xdpi * (1.0f / 25.4f)
-        }
-        return 0f
     }
 }
