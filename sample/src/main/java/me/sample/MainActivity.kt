561873160977ed03831c7b443c08112a215b2762
haowen <haowenhello@gmial.com>
2017-09-25 11:30:11 +0800

    remove test code

--- sample/src/main/java/me/sample/MainActivity.kt
+++ sample/src/main/java/me/sample/MainActivity.kt
@@ -2,25 +2,11 @@ package me.sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import me.utils.DeviceUtil
-import me.utils.Utils
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Utils.init(this)
-
-        println("===============>>  ${DeviceUtil.isRooted()}")
-
-        println("===============>>  ${DeviceUtil.getSDKVersion()}")
-
-        println("===============>>  ${DeviceUtil.getAndroidID()}")
-
-        println("===============>>  ${DeviceUtil.getModel()}")
-
-        println("===============>>  ${DeviceUtil.getManufacturer()}")
     }
 }
